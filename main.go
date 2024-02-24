aa5bcc44e39ea2027d8eddb961b586c2061200e7
sudeep162002 <80563363+sudeep162002@users.noreply.github.com>
2024-02-24 15:56:21 +0530

    feature:go gin proxy for lamda

--- main.go
+++ main.go
@@ -1,10 +1,13 @@
 package main
 
 import (
+	"context"
 	"fmt"
 	"net/http"
-	"os"
 
+	"github.com/aws/aws-lambda-go/events"
+	"github.com/aws/aws-lambda-go/lambda"
+	ginadapter "github.com/awslabs/aws-lambda-go-api-proxy/gin"
 	db "github.com/sudeep162002/ims-go-backend/db"
 	"github.com/sudeep162002/ims-go-backend/middleware"
 	user_routes "github.com/sudeep162002/ims-go-backend/routes"
@@ -13,7 +16,9 @@ import (
 	"github.com/gin-gonic/gin"
 )
 
-func main() {
+var ginLambda *ginadapter.GinLambda
+
+func init() {
 	r := gin.New()
 
 	// make db connection
@@ -44,16 +49,6 @@ func main() {
 		c.JSON(http.StatusOK, response)
 	})
 
-	r.GET("/ims-backend", func(c *gin.Context) {
-		// Create a JSON object
-		response := gin.H{
-			"message": "welcome to ims backend go version",
-		}
-		fmt.Println(response)
-		// Return the JSON response with a custom status code
-		c.JSON(http.StatusOK, response)
-	})
-
 	// health check route
 	r.GET("/health", func(c *gin.Context) {
 		// Create a JSON object
@@ -65,6 +60,15 @@ func main() {
 		c.JSON(http.StatusOK, response)
 	})
 
-	// Run the server on port 3000
-	r.Run(":" + os.Getenv("PORT"))
+	ginLambda = ginadapter.New(r)
+
+}
+
+func Handler(ctx context.Context, req events.APIGatewayProxyRequest) (events.APIGatewayProxyResponse, error) {
+	// If no name is provided in the HTTP request body, throw an error
+	return ginLambda.ProxyWithContext(ctx, req)
+}
+
+func main() {
+	lambda.Start(Handler)
 }
